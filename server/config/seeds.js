b626d924eee01a5dea1e43bb6062e264e9ebc996
Brian Alegre <brialegre@yahoo.com>
2023-08-15 21:55:52 -0700

    Created seeds for Currency and Flight.  Successfully seeded

--- server/config/seeds.js
+++ server/config/seeds.js
@@ -7,8 +7,6 @@ db.once("open", async () => {
   // USERS
   // Delete all users
   await User.deleteMany();
-
-  // Log to backend console
   console.log("*** DELETED ALL USERS ***");
 
   // Create users
@@ -29,7 +27,48 @@ db.once("open", async () => {
   // Log to backend console
   console.log("*** SUCCESSFULLY SEEDED USERS ***");
 
-  // LOG TO BACKEND CONSOLE
+
+  // CURRENCIES
+  // Delete all currencies
+  await Currency.deleteMany();
+  console.log("*** DELETED ALL CURRENCIES ***");
+
+  // Create currencies
+  const currencyOne = await Currency.create({
+    yen: 100,
+    usd: 1,
+    date: currentDate
+  })
+
+  // Log to backend console
+  console.log("*** SUCCESSFULLY SEEDED CURRENCIES ***");
+
+
+  // FLIGHTS
+  // Delete all flights
+  await Flight.deleteMany();
+  console.log("*** DELETED ALL FLIGHTS ***");
+
+  // Create flights
+  const flightOne = await Flight.create({
+    flightNumber: "AA123",
+    airline: "ANA",
+    departureLocation: "LAX",
+    arrivalLocation: "HND",
+    dateDepature: "2021-01-01",
+    dateArrival: "2021-01-02",
+    timeDeparture: "12:00",
+    price: 1000,
+    returnDate: "2021-01-03",
+    returnFlightTime: "12:00",
+    returnFlightAirline: "ANA",
+    returnFlightNumber: "AA123",
+  })
+
+  // Log to backend console
+  console.log("*** SUCCESSFULLY SEEDED FLIGHTS ***");
+
+  // Log to backend console
   console.log("*** SUCCESSFULLY SEEDED ***");
 
 
